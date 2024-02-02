41d490694763309a309c9ae7a77bf1427aa3664a
iEncode Tech Team <iencodetech@gmail.com>
2024-02-03 00:52:10 +0530

    updated

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,7 @@
 $(function(){
     function timer(settings){
         var config = {
-            endDate: '2030-01-20 03:00',
+            endDate: '2024-02-26 00:00',
             timeZone: 'Europe/Dublin',
             hours: $('#hours'),
             minutes: $('#minutes'),
